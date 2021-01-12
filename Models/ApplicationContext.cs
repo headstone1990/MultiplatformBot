412ea2ca5b049a0ead0e6f3c2804edf2b4853772
HeadStone <feuerschwanz1990@gmail.com>
2021-01-12 12:45:11 +0300

    Add example for EntityFramework>General C# code>Angular interaction

---
+++ Models/ApplicationContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace VkBot.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) 
+            : base(options)
+        { 
+            Database.EnsureCreated();
+        }
+ 
+        public DbSet<Product> Products { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -6,9 +6,11 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using VkBot.Models;
 using VkNet;
 using VkNet.Abstractions;
 using VkNet.Model;
@@ -23,18 +25,22 @@ namespace VkBot
         }
 
         public IConfiguration Configuration { get; }
-        
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            string connectionString = "Data Source=identifier.db;Cache=Shared";
+            services.AddDbContext<ApplicationContext>(options => options.UseSqlite(connectionString));
+            // string connectionString = "server=eu-cdbr-west-03.cleardb.net;user=b55ff856a44a07;password=41f84f28;database=heroku_7d6ab67ff3e4e29";
+            // services.AddDbContext<ApplicationContext>(options =>
+            //     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
             services.AddControllers();
             services.AddControllers().AddNewtonsoftJson();
-            services.AddSpaStaticFiles(configuration =>
+            services.AddSpaStaticFiles(configuration => { configuration.RootPath = "ClientApp/dist"; });
+            services.AddSingleton<IVkApi>(sp =>
             {
-                configuration.RootPath = "ClientApp/dist";
-            });
-            services.AddSingleton<IVkApi>(sp => {
                 var api = new VkApi();
                 api.Authorize(new ApiAuthParams
                 {
@@ -51,6 +57,7 @@ namespace VkBot
             {
                 app.UseDeveloperExceptionPage();
             }
+
             if (!env.IsDevelopment())
             {
                 app.UseSpaStaticFiles();
@@ -58,14 +65,13 @@ namespace VkBot
 
             app.UseHttpsRedirection();
             app.UseRouting();
-            app.UseAuthorization();
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
             app.UseStaticFiles();
 
             app.UseSpa(spa =>
             {
                 spa.Options.SourcePath = "ClientApp";
- 
+
                 if (env.IsDevelopment())
                 {
                     spa.UseAngularCliServer(npmScript: "start");
